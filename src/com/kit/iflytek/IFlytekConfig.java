acd762eea096d886fc2402caf2cd79b7d3ccc701
Zhao <laozhao1005@gmail.com>
2017-07-22 01:13:49 +0800

    这样可以

--- src/com/kit/iflytek/IFlytekConfig.java
+++ src/com/kit/iflytek/IFlytekConfig.java
@@ -1,11 +1,18 @@
 package com.kit.iflytek;
 
+import android.graphics.Bitmap;
+
 import com.kit.config.AppConfig;
 
 /**
  * Created by Zhao on 16/7/26.
  */
 public class IFlytekConfig implements AppConfig.IAppConfig {
+
+    @Override
+    public Bitmap.Config getPicLevel() {
+        return Bitmap.Config.ARGB_4444;
+    }
 
     @Override
     public boolean isShowLog() {
