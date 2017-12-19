e40072b45ee7ec801e2056ba05a2330542ae5d66
joeyzhao <joeyzhao1005@gmail.com>
2017-12-19 19:42:27 +0800

    kotlin

--- src/com/kit/iflytek/mangaer/AIUI.kt
+++ src/com/kit/iflytek/mangaer/AIUI.kt
@@ -2,6 +2,7 @@ package com.kit.iflytek.mangaer
 
 import android.app.Activity
 import android.app.Application
+import android.content.Context
 import com.iflytek.aiui.AIUIAgent
 import com.iflytek.aiui.AIUIConstant
 import com.iflytek.aiui.AIUIListener
@@ -15,50 +16,22 @@ import java.io.IOException
 class AIUI private constructor() {
 
 
-    private val aiuiParams: String
-        get() {
-            var params = ""
-
-            val assetManager = ResWrapper.getInstance().resources.assets
-            try {
-                val ins = assetManager.open("cfg/aiui_phone.cfg")
-                val buffer = ByteArray(ins.available())
-
-                ins.read(buffer)
-                ins.close()
-                params = buffer.toString()
-//                params = String(buffer)
-            } catch (e: IOException) {
-                e.printStackTrace()
-            }
-
-            return params
-        }
-
-
-    private var mAIUIListener: AIUIListener? = null
-
-    private val mAIUIState = AIUIConstant.STATE_IDLE
-
-    private var mAIUIAgent: AIUIAgent? = null
-
-    private var activity: Activity? = null
-
-
+    @Synchronized
     fun listen() {
         val wakeupMsg = AIUIMessage(AIUIConstant.CMD_WAKEUP, 0, 0, "", null)
-        mAIUIAgent!!.sendMessage(wakeupMsg)
+        mAIUIAgent?.sendMessage(wakeupMsg)
     }
 
 
     /**
      * 开始语音理解
      */
-    fun startVoiceNlp(): AIUI {
+    @Synchronized
+    fun startVoiceNlp(context: Context): AIUI {
 
         ZogUtils.i("start voice nlp")
 
-        if (!checkAIUIAgent()) {
+        if (!checkAIUIAgent(context)) {
             return this
         }
 
@@ -66,13 +39,13 @@ class AIUI private constructor() {
         // 默认为oneshot 模式，即一次唤醒后就进入休眠，如果语音唤醒后，需要进行文本语义，请将改段逻辑copy至startTextNlp()开头处
         if (AIUIConstant.STATE_WORKING != this.mAIUIState) {
             val wakeupMsg = AIUIMessage(AIUIConstant.CMD_WAKEUP, 0, 0, "", null)
-            mAIUIAgent!!.sendMessage(wakeupMsg)
+            mAIUIAgent?.sendMessage(wakeupMsg)
         }
 
         // 打开AIUI内部录音机，开始录音
         val params = "sample_rate=16000,data_type=audio"
         val writeMsg = AIUIMessage(AIUIConstant.CMD_START_RECORD, 0, 0, params, null)
-        mAIUIAgent!!.sendMessage(writeMsg)
+        mAIUIAgent?.sendMessage(writeMsg)
         return this
     }
 
@@ -89,24 +62,25 @@ class AIUI private constructor() {
     /**
      * 停止语音理解
      */
-    fun stopVoiceNlp() {
+    @Synchronized
+    fun stopVoiceNlp(context: Context) {
 
         ZogUtils.i("stop voice nlp")
 
-        if (!checkAIUIAgent()) {
+        if (!checkAIUIAgent(context)) {
             return
         }
         // 停止录音
         val params = "sample_rate=16000,data_type=audio"
         val stopWriteMsg = AIUIMessage(AIUIConstant.CMD_STOP_RECORD, 0, 0, params, null)
 
-        mAIUIAgent!!.sendMessage(stopWriteMsg)
+        mAIUIAgent?.sendMessage(stopWriteMsg)
     }
 
 
-    private fun checkAIUIAgent(): Boolean {
+    private fun checkAIUIAgent(context: Context): Boolean {
 
-        if (activity == null) {
+        if (context == null) {
             ZogUtils.e("activity not set")
         }
 
@@ -116,7 +90,7 @@ class AIUI private constructor() {
 
         if (null == mAIUIAgent) {
             ZogUtils.i("create aiui agent")
-            mAIUIAgent = AIUIAgent.createAgent(activity, aiuiParams, mAIUIListener)
+            mAIUIAgent = AIUIAgent.createAgent(context, aiuiParams, mAIUIListener)
             val startMsg = AIUIMessage(AIUIConstant.CMD_START, 0, 0, null, null)
             mAIUIAgent!!.sendMessage(startMsg)
         }
@@ -133,17 +107,48 @@ class AIUI private constructor() {
     /**
      * 销毁
      */
+    @Synchronized
     fun destory() {
         if (null != this.mAIUIAgent) {
             val stopMsg = AIUIMessage(AIUIConstant.CMD_STOP, 0, 0, null, null)
-            mAIUIAgent!!.sendMessage(stopMsg)
+            mAIUIAgent?.sendMessage(stopMsg)
 
-            this.mAIUIAgent!!.destroy()
-            this.mAIUIAgent = null
+            try {
+                this.mAIUIAgent?.destroy()
+            } catch (e: Exception) {
+            }
         }
-        activity = null
-        mInstance = null
+
+        this.mAIUIAgent = null
     }
+
+
+
+    private val aiuiParams: String
+        get() {
+            var params = ""
+
+            val assetManager = ResWrapper.getInstance().resources.assets
+            try {
+                val ins = assetManager.open("cfg/aiui_phone.cfg")
+                val buffer = ByteArray(ins.available())
+
+                ins.read(buffer)
+                ins.close()
+//                params = buffer.toString()
+                params = String(buffer)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+
+            return params
+        }
+
+    private var mAIUIListener: AIUIListener? = null
+
+    private val mAIUIState = AIUIConstant.STATE_IDLE
+
+    private var mAIUIAgent: AIUIAgent? = null
 
 
     companion object {
@@ -151,21 +156,10 @@ class AIUI private constructor() {
             SpeechUtility.createUtility(application, "appid=" + iflytekAppId)
         }
 
-        fun with(activity: Activity): AIUI? {
-            if (mInstance == null) {
-                mInstance = AIUI()
-            }
-            mInstance?.activity = activity
-
-            return mInstance
-        }
-
         fun get(): AIUI? {
-            return mInstance;
+            return INSTANCE
         }
 
-        private var mInstance: AIUI? = null
+        val INSTANCE: AIUI by lazy { AIUI() }
     }
-
-
 }
